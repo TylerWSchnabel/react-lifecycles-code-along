8038e92b8279c2c63b51b54b278ddbb17706f3da
Tyler Schnabel <tylerwschnabel@gmail.com>
2022-10-06 13:39:29 -0700

    Did the code along

---
+++ src/components/Counter.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const ErrorComponent = () => <div>{props.ignore}</div>
+
+export default class Counter extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            counter: 0,
+            seed: 0
+        }
+
+        this.increment = () => this.setState({counter: this.state.counter +1})
+        this.decrement = () => this.setState({counter: this.state.counter - 1})
+    }
+
+    static getDerivedStateFromProps(props, state){
+        if(props.seed && state.seed !== props.seed){
+            return{
+                seed: props.seed,
+                counter: props.seed
+            }
+        }
+        return null 
+    }
+
+    componentDidMount(){
+        console.log('Component Did Mount')
+        console.log('---------------------')
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        
+        if(nextProps.ignoreProp 
+            && this.props.ignoreProp!== nextProps.ignoreProp){
+                console.log('Should Component Update -- No')
+            console.log('---------------------')
+                return false
+        }
+        console.log('Should Component Update - Yes')
+        console.log('---------------------')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('Get Snapshot Before Update')
+        console.log('---------------------')
+        return null
+    }
+
+    render () {
+        console.log('Render')
+        
+        if(this.props.showErrorComponent && this.state.error){
+            return <div>We encountered an Error! {this.state.error.message}</div>
+        }
+
+        return <div>
+            <button onClick={this.increment}>Increment</button>
+            <button onClick={this.decrement}>Decrement</button>
+            <div className="counter">
+                Counter: {this.state.counter}
+            </div>
+            {this.props.showErrorComponent ? <ErrorComponent/> : null}
+        </div>
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log("Component did Update")
+        console.log('---------------------')
+    }
+
+    componentWillUnmount(){
+        console.log('Component Will Unmount')
+        console.log('---------------------')
+    }
+
+    componentDidCatch(error, info){
+        console.log('Component Did Catch')
+
+        this.setState({error, info})
+    }
+}
